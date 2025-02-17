db27f5521edbca949b7ae7706a61ab8f14af2d51
Yauhen Nohach Dev <jenaynogach1@mail.ru>
2025-02-17 17:05:55 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -7,49 +7,53 @@ import AboutPage from './AboutPage.tsx';
 // @ts-ignore
 import UserPage from './UserPage.tsx';
 // @ts-ignore
-import UserDetailPage from './UserDetailPage.tsx'; // Импортируем новый компонент
-
+import UserDetailPage from './UserDetailPage.tsx';
 // @ts-ignore
 import UserListPage from './UserListPage.tsx';
-
 // @ts-ignore
 import AddProperty from './AddProperty.tsx';
 
-
 const App = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/user">Add User</Link>
-          </li>
-          <li>
-            <Link to="/users">User List</Link>
-          </li>
-          <li>
-            <Link to="/addProperty">Add Property</Link>
-          </li>
-        </ul>
-      </nav>
+      <Router>
+        <header
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: '2rem',
+              padding: '15px 0',
+              backgroundColor: '#f0f0f0',
+            }}
+        >
+          <Link to="/" style={{ textDecoration: 'none', color: '#333' }}>
+            Home
+          </Link>
+          <Link to="/about" style={{ textDecoration: 'none', color: '#333' }}>
+            About
+          </Link>
+          <Link to="/user" style={{ textDecoration: 'none', color: '#333' }}>
+            Add User
+          </Link>
+          <Link to="/users" style={{ textDecoration: 'none', color: '#333' }}>
+            User List
+          </Link>
+          <Link to="/addProperty" style={{ textDecoration: 'none', color: '#333' }}>
+            Add Property
+          </Link>
+        </header>
 
-      <Routes>
-        {/* Основные маршруты */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/user" element={<UserPage />} />
-        <Route path="/users" element={<UserListPage />} />
-        {/* Динамический маршрут для страницы студента по ID */}
-        <Route path="/user/:id" element={<UserDetailPage />} />
-        <Route path="addProperty" element={<AddProperty />} />
-      </Routes>
-    </Router>
+        <Routes>
+          {/* Основные маршруты */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/user" element={<UserPage />} />
+          <Route path="/users" element={<UserListPage />} />
+          {/* Динамический маршрут для страницы студента по ID */}
+          <Route path="/user/:id" element={<UserDetailPage />} />
+          <Route path="/addProperty" element={<AddProperty />} />
+        </Routes>
+      </Router>
   );
 };
 
