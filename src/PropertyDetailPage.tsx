8ad681df26dd2da54f119518722a1afda9f1cd35
Yauhen Nohach Dev <jenaynogach1@mail.ru>
2025-05-01 10:16:19 +0300

    update property v4.0

--- src/PropertyDetailPage.tsx
+++ src/PropertyDetailPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
 import "./property-detail.css";
+import "./property.css";
 
 interface Property {
     id: number;
