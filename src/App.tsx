31bbe05e50153965623e8eecf13a6a940a2e17c6
Yauhen Nohach Dev <jenaynogach1@mail.ru>
2025-05-07 10:48:36 +0300

    add app css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./app.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 // @ts-ignore
 import Home from "./Home.tsx";
